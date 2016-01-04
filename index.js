9d037a4013a1fd83049470e495a2c1277ea77c5c
skylerspaeth <skylerspaeth.scc@me.com>
2016-01-03 19:51:19 -0600

    now reads JSON properly

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var app = express();
-var _ = require('underscore');
 var io = require('socket.io').listen(8081);
 
 app.use(express.static('public'));
 
 var server = app.listen(3000, function () {
   var port = server.address().port;
-  console.log('Example app listening at', port);
+  console.log('rovctl listening on port', port);
 
 io.on('connection', socket => socket.on('message', msg => newValues(msg)));
 
