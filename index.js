1f329ebd7bfa8cf72a5e1aeaee9e276023d71846
skylerspaeth <skylerspaeth.scc@me.com>
2016-01-03 19:48:46 -0600

    now reads JSON properly

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var _ = require('underscore');
 var io = require('socket.io').listen(8081);
 
 app.use(express.static('public'));
@@ -11,7 +12,8 @@ var server = app.listen(3000, function () {
 io.on('connection', socket => socket.on('message', msg => newValues(msg)));
 
 function newValues(msg) {
-	console.log(msg);
+	var data = JSON.parse(msg);
+	console.log(data.alpha, data.beta, data.gamma);
 };
 
 });
